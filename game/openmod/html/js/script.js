54d115da2946bf1f1a3003e18fa5fca967a81393
ThePixelMoon <yourlocalmoon7@gmail.com>
2024-11-30 18:47:56 +0200

    update: sounds
    because yes

--- game/openmod/html/js/script.js
+++ game/openmod/html/js/script.js
@@ -2,11 +2,15 @@ const buttons = document.querySelectorAll('.top-btn');
 
 buttons.forEach(button => {
   button.addEventListener('mouseenter', () => {
-    const hoverSound = new Audio('assets/ui/hover.mp3');
-      
+    const hoverSound = new Audio('assets/ui/hover.wav');
     hoverSound.preload = 'auto';
-      
     hoverSound.play().catch(error => { }); // don't catch an error
+  });
+
+  button.addEventListener('click', () => {
+    const clickSound = new Audio('assets/ui/click.wav');
+    clickSound.preload = 'auto';
+    clickSound.play().catch(error => { }); // don't catch an error
   });
 });
 
